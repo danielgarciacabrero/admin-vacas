396281bb6fa145c0c4438d10670335ab94fc74ad
danielgarciacabrero <danielgarciacabrero@gmail.com>
2026-04-21 11:45:34 +0200

    prueba4-notificacion

--- src/api/client.js
+++ src/api/client.js
@@ -1,29 +1,11 @@
 import axios from 'axios';
 
 const client = axios.create({
-  baseURL: 'https://efrw7l4wtj.execute-api.eu-south-2.amazonaws.com/dev/', // La que sale al hacer sls deploy
-});
-
-// Interceptor para inyectar el Token de Cognito en cada petición
-client.interceptors.request.use((config) => {
-
-  import axios from 'axios';
-
-const client = axios.create({
   baseURL: 'https://efrw7l4wtj.execute-api.eu-south-2.amazonaws.com/dev/',
 });
 
 // Interceptor para inyectar el Token de Cognito en cada petición
 client.interceptors.request.use((config) => {
-  const token = localStorage.getItem('idToken');
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
-
-export default client;
-  
   const token = localStorage.getItem('idToken');
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
