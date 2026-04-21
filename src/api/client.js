a56f0ae1925527154250d21452857239c11ee28c
danielgarciacabrero <danielgarciacabrero@gmail.com>
2026-04-21 09:48:27 +0200

    prueba2-notificacion

--- src/api/client.js
+++ src/api/client.js
@@ -7,7 +7,22 @@ const client = axios.create({
 // Interceptor para inyectar el Token de Cognito en cada petición
 client.interceptors.request.use((config) => {
 
-  config.headers['x-api-key'] = 'EO13oAwlg0aWd3b3M0HGU1gCyWshY5PH61DJTYkd';
+  import axios from 'axios';
+
+const client = axios.create({
+  baseURL: 'https://efrw7l4wtj.execute-api.eu-south-2.amazonaws.com/dev/',
+});
+
+// Interceptor para inyectar el Token de Cognito en cada petición
+client.interceptors.request.use((config) => {
+  const token = localStorage.getItem('idToken');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export default client;
   
   const token = localStorage.getItem('idToken');
   if (token) {
